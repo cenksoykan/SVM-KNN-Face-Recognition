3cc6c45ca10f111388ee00907828fc65992d51a2
Cenk SOYKAN <cenksoykan@cenksoykan.com>
2018-04-10 01:33:44 +0300

    Initial commit

---
+++ README.md
@@ -0,0 +1,70 @@
+# Report
+
+## Task 1: Classification using PCA + 1NN with 5-fold cross validation
+
+For PCA:
+
+* Centerized the images
+* Calculated Covariance matrix
+* Calculated eigen values and eigen vectors
+* Sorted the eigen values in descending order and then sorted the eigen vectors by the same index
+* Took the first 70 principal components and plotted the data into new dimensions.
+
+For 1NN:
+
+* Calculated the euclidean distance for each test image with every train image
+* Sorted the distance in ascending order and took the 1st one as the result
+* Compare the result with original class and computed the accuracy
+
+After the 5-fold cross validation, the average accuracy for **PCA + 1NN** was 96.75%
+
+## Task 2: Repeating Task 1 after resizing the images from 112 x 92 to 56 x 46
+
+The average accuracy after **Resizing Images + PCA + 1NN** was 96.0%
+
+Comparing this with the original dimensions accuracy, there does not seem to be much change in the accuracy.
+
+## Task 3: Classification using LDA + 1NN with 5-fold cross validation
+
+For LDA:
+
+* Calculated class wise mean vectors
+* Calculated in-class scatter matrix
+* Calculated between-class scatter matrix
+* Calculated eigen values and eigen vectors
+* Sorted the eigen values in descending order and then sorted the eigen vectors by the same index
+* Took the first 70 principal components and plotted the data into new dimensions.
+
+After the 5-fold cross validation, the average accuracy for **LDA + 1NN** was 96.0%
+
+## Task 4: Classification using PCA + LDA + 1NN with 5-fold cross validation
+
+After the 5-fold cross validation, the average accuracy for **PCA + LDA + 1NN** was 98.0%
+
+Comparing this with previous results we can see that PCA before LDA helps increasing the accuracy of 1NN.
+
+## Task 5: Classification using SVM with 5-fold cross validation
+
+For SVM:
+
+* Generated Gramian Matrix
+* Calculated values of P, q, A, b, G, h and solved quadratic equation
+* Determined support vectors
+* Calculatied bias and weights
+* Prediction was done using one-vs-rest approach
+
+After the 5-fold cross validation, the average accuracy for **SVM** was 97.4%
+
+## Task 6: Classification using PCA + SVM with 5-fold cross validation
+
+After the 5-fold cross validation, the average accuracy for **PCA + SVM** was 92.7%
+
+Comparing this with previous SVM results we can see that PCA decreases the accuracy of SVM drastically.
+
+---
+
+Answering the question whether KNN or SVM is more sensitive to high dimensionality of data.
+
+**Answer:** SVM is more sensitive to high dimensionality as reducing the dimensions through PCA affects negatively to SVM, on the other hand PCA and LDA helps KNN to increase its accuracy with lower dimensions.
+
+This is because PCA retrieves the features with highest information gain and discards the other, but SVM does not work feature wise, it relies on data-space dimensions which is reduced by PCA.
--- faceRec.py
+++ faceRec.py
@@ -1,33 +1,32 @@
-# Dependencies
-# Please install
-# 'CVXOPT' library for python used to solve quadratic equations
-# 'sklearn' library used for shuffling the data and cross_validation
-# 'scipy' library used for reading images
-# 'numpy' library used for various calculations
+"""
+Dependencies ->
+    Please install:
+    'CVXOPT' library for python used to solve quadratic equations
+    'scipy' library used for reading images
 
-# References ->
-# https://www.youtube.com/user/sentdex/videos
-# http://cvxopt.org/examples/tutorial/qp.html
-# https://pythonprogramming.net/soft-margin-kernel-cvxopt-svm-machine-learning-tutorial/
-# https://pythonprogramming.net/coding-k-nearest-neighbors-machine-learning-tutorial/
-# http://stackoverflow.com/questions/13224362/principal-component-analysis-pca-in-python
-# http://sebastianraschka.com/Articles/2014_python_lda.html
-# http://stackoverflow.com for various syntax doubts.
+References ->
+    https://www.youtube.com/user/sentdex/videos
+    http://cvxopt.org/examples/tutorial/qp.html
+    https://pythonprogramming.net/soft-margin-kernel-cvxopt-svm-machine-learning-tutorial/
+    https://pythonprogramming.net/coding-k-nearest-neighbors-machine-learning-tutorial/
+    http://stackoverflow.com/questions/13224362/principal-component-analysis-pca-in-python
+    http://sebastianraschka.com/Articles/2014_python_lda.html
+"""
 
 import os
-import numpy as np
+import math
 import cvxopt
 import scipy.misc
-import math
-from matplotlib import pyplot
-from sklearn.utils import shuffle
-from sklearn import cross_validation
+import numpy as np
+from sklearn import model_selection
+
+#from sklearn.utils import shuffle
+# from matplotlib import pyplot
 
 
-class Support_Vector_Machine():
-
+class SupportVectorMachine():
     def __init__(self):
-        # Defining global variables
+        """Defining global variables"""
         self.c = 100.0
         self.lm = None
         self.sv_X = None
@@ -36,22 +35,25 @@ class Support_Vector_Machine():
         self.w = None
 
     def train(self, x, y):
-        # Extracting sample and feature lengths
+        """Extracting sample and feature lengths"""
         sample_len, feature_len = x.shape
 
         # Generating Gramian Matrix
         M = np.zeros((sample_len, sample_len))
         for i in range(sample_len):
             for j in range(sample_len):
-                M[i,j] = np.dot(x[i], x[j])
+                M[i, j] = np.dot(x[i], x[j])
 
         # Calculating values of P, q, A, b, G, h
         P = cvxopt.matrix(np.outer(y, y) * M)
         q = cvxopt.matrix(np.ones(sample_len) * -1)
         A = cvxopt.matrix(y, (1, sample_len), 'd')
         b = cvxopt.matrix(0.0)
-        G = cvxopt.matrix(np.vstack((np.diag(np.ones(sample_len) * -1), np.identity(sample_len))))
-        h = cvxopt.matrix(np.hstack((np.zeros(sample_len), np.ones(sample_len) * self.c)))
+        G = cvxopt.matrix(
+            np.vstack((np.diag(np.ones(sample_len) * -1),
+                       np.identity(sample_len))))
+        h = cvxopt.matrix(
+            np.hstack((np.zeros(sample_len), np.ones(sample_len) * self.c)))
 
         # Solving quadratic equation
         # You can set 'show_progress' to True to see cvxopt output
@@ -70,7 +72,8 @@ class Support_Vector_Machine():
         # Calculating bias
         self.b = 0.0
         for i in range(len(self.lm)):
-            self.b = self.b + self.sv_y[i] - np.sum(self.lm * self.sv_y * M[index[i],sv])
+            self.b = self.b + self.sv_y[i] - np.sum(
+                self.lm * self.sv_y * M[index[i], sv])
         self.b /= len(self.lm)
 
         # Calculating weights
@@ -83,14 +86,14 @@ class Support_Vector_Machine():
 
 
 def PCA(X_train, X_test, k):
-    # Centerize the images
+    """Centerize the images"""
     X_train -= np.mean(X_train, axis=0)
     X_test -= np.mean(X_test, axis=0)
 
-    print 'Calculating Covariance matrix'
+    print("Calculating Covariance matrix")
     CovM = np.cov(X_train.T)
 
-    print 'Calculating eigen values and eigen vectors, please wait...'
+    print("Calculating eigen values and eigen vectors, please wait...")
     evals, evecs = np.linalg.eigh(CovM)
     # Sort the eigen values in descending order and then sorted the eigen vectors by the same index
     idx = np.argsort(evals)[::-1][:k]
@@ -98,50 +101,51 @@ def PCA(X_train, X_test, k):
 
     # Can uncomment for plotting eigen values graph
     # evals = evals[idx]
-    # pyplot.plot(evals)
-    # pyplot.show()
+    # # pyplot.show()
     return np.dot(evecs.T, X_train.T).T, np.dot(evecs.T, X_test.T).T
+    # # pyplot.plot(evals)
 
 
 def LDA(X_train, y_train, X_test, k):
 
-    print 'Calculating class wise mean vectors'
-    m , n = X_train.shape
+    print("Calculating class wise mean vectors")
+    m, n = X_train.shape
     class_wise_mean = []
-    for i in range(1,41):
-        idx = np.where(y_train==i)
+    for i in range(1, 41):
+        idx = np.where(y_train == i)
         class_wise_mean.append(np.mean(X_train[idx], axis=0))
 
-    print 'Calculating within-class scatter matrix'
+    print("Calculating within-class scatter matrix")
     within_SM = np.zeros((n, n))
     for i, mean_vector in zip(range(1, 41), class_wise_mean):
         class_wise_M = np.zeros((n, n))
-        idx = np.where(y_train==i)
+        idx = np.where(y_train == i)
         for img in X_train[idx]:
             img, mean_vector = img.reshape(n, 1), mean_vector.reshape(n, 1)
             class_wise_M += (img - mean_vector).dot((img - mean_vector).T)
         within_SM += class_wise_M
 
-    print 'Calculating between-class scatter matrix'
+    print("Calculating between-class scatter matrix")
     total_mean = np.mean(X_train, axis=0)
     between_SM = np.zeros((n, n))
     for i, mean_vector in enumerate(class_wise_mean):
-        idx = np.where(y_train==i+1)
+        idx = np.where(y_train == i + 1)
         cnt = X_train[idx].shape[0]
         mean_vector = mean_vector.reshape(n, 1)
         total_mean = total_mean.reshape(n, 1)
-        between_SM += cnt * (mean_vector - total_mean).dot((mean_vector - total_mean).T)
+        between_SM += cnt * (mean_vector - total_mean).dot(
+            (mean_vector - total_mean).T)
 
-    print 'Calculating eigen values and eigen vectors, please wait...'
+    print("Calculating eigen values and eigen vectors, please wait...")
     evals, evecs = np.linalg.eigh(np.linalg.inv(within_SM).dot(between_SM))
     idx = np.argsort(evals)[::-1][:k]
     evecs = evecs[:, idx]
 
     # Can uncomment for plotting eigen values graph
     # evals = evals[idx]
-    # pyplot.plot(evals)
-    # pyplot.show()
+    # # pyplot.show()
     return np.dot(evecs.T, X_train.T).T, np.dot(evecs.T, X_test.T).T
+    # # pyplot.plot(evals)
 
 
 def kNN(X_train, y_train, X_test, y_test):
@@ -168,20 +172,22 @@ def kNN(X_train, y_train, X_test, y_test):
         c = np.sum(predicted == correct)
         total_corr += c
         i += 1
-    print 'Accuracy is ', float(total_corr)/80*100
-    return float(total_corr)/80*100
+    print("Accuracy is ", float(total_corr) / 80 * 10)
+    return float(total_corr) / 80 * 100
 
 
 if __name__ == "__main__":
+
     def img_input(resize=False):
-        X,y = [], []
-        path, direc, docs = os.walk("orl_faces").next()
+        X, y = [], []
+        path, direc, docs = os.walk("orl_faces").__next__()
         direc.sort()
         # Iterating through each subject
         for subject in direc:
-            files = os.listdir(path+'/'+subject)
+            files = os.listdir(path + '/' + subject)
             for file in files:
-                img = scipy.misc.imread(path+'/'+subject+'/'+file).astype(np.float32)
+                img = scipy.misc.imread(
+                    path + '/' + subject + '/' + file).astype(np.float32)
                 if resize:
                     img = scipy.misc.imresize(img, (56, 46)).astype(np.float32)
                 X.append(img.reshape(-1))
@@ -191,47 +197,49 @@ if __name__ == "__main__":
         return X, y
 
     def cross_val(X, y, DR=None, Alg=None, All=False):
-        X, y = shuffle(X, y)
-        kf = cross_validation.KFold(len(y), n_folds=5)
+        kf = model_selection.KFold(n_splits=5, shuffle=True)
         avg_acc_p = []
         avg_acc_l = []
         avg_acc_pl = []
         avg_acc_svm = []
         avg_acc_svm_pca = []
         fld = 1
-        for train, test in kf:
-            X_train, X_test, y_train, y_test = X[train], X[test], y[train], y[test]
+        for train, test in kf.split(y):
+            X_train, X_test, y_train, y_test = X[train], X[test], y[train], y[
+                test]
 
             if DR == 'PCA' and Alg == '1NN' or All:
-                print '\nRunning Fold', fld, 'for PCA + 1NN'
+                print("\nRunning Fold ", fld, " for PCA + 1NN")
                 X_train_pca, X_test_pca = PCA(X_train, X_test, 70)
                 avg_acc_p.append(kNN(X_train_pca, y_train, X_test_pca, y_test))
                 if not All:
                     fld += 1
 
             if DR == 'LDA' and Alg == '1NN' or All:
-                print '\nRunning Fold', fld, 'for LDA + 1NN'
+                print("\nRunning Fold ", fld, " for LDA + 1NN")
                 X_train_lda, X_test_lda = LDA(X_train, y_train, X_test, 70)
                 avg_acc_l.append(kNN(X_train_lda, y_train, X_test_lda, y_test))
                 if not All:
                     fld += 1
 
             if DR == 'PCA+LDA' and Alg == '1NN' or All:
-                print '\nRunning Fold', fld, 'for PCA + LDA + 1NN'
+                print("\nRunning Fold ", fld, " for PCA + LDA + 1NN")
                 X_train_pca, X_test_pca = PCA(X_train, X_test, 70)
-                X_train_pca_lda, X_test_pca_lda = LDA(X_train_pca, y_train, X_test_pca, 25)
-                avg_acc_pl.append(kNN(X_train_pca_lda, y_train, X_test_pca_lda, y_test))
+                X_train_pca_lda, X_test_pca_lda = LDA(X_train_pca, y_train,
+                                                      X_test_pca, 25)
+                avg_acc_pl.append(
+                    kNN(X_train_pca_lda, y_train, X_test_pca_lda, y_test))
                 if not All:
                     fld += 1
 
             if DR is None and Alg == 'SVM' or All:
-                print '\nRunning Fold', fld, 'for SVM'
+                print("\nRunning Fold ", fld, " for SVM")
                 # Defining svm model
-                svm = Support_Vector_Machine()
-                y_train_ovr = [None]*len(y_train)
-                y_test_ovr = [None]*len(y_test)
+                svm = SupportVectorMachine()
+                y_train_ovr = [None] * len(y_train)
+                y_test_ovr = [None] * len(y_test)
                 accuracies = 0
-                print 'Running SVM. Please wait...'
+                print("Running SVM. Please wait...")
                 for i in range(1, 41):
                     # Setting the selected class as '1' and rest as '-1' depicting the One vs Rest classification.
                     for j in range(0, 320):
@@ -249,21 +257,21 @@ if __name__ == "__main__":
                     svm.train(X_train, y_train_ovr)
                     predict_class = svm.predict(X_test)
                     c = np.sum(predict_class == y_test_ovr)
-                    accuracies += float(c)/len(predict_class)*100
-                accuracy = math.ceil(accuracies/40)
-                print 'Accuracy is ', accuracy
+                    accuracies += float(c) / len(predict_class) * 100
+                accuracy = math.ceil(accuracies / 40)
+                print("Accuracy is ", accuracy)
                 avg_acc_svm.append(accuracy)
                 if not All:
                     fld += 1
 
             if DR == 'PCA' and Alg == 'SVM' or All:
-                print '\nRunning Fold', fld, 'for PCA + SVM'
+                print("\nRunning Fold ", fld, " for PCA + SVM")
                 X_train_pca, X_test_pca = PCA(X_train, X_test, 70)
-                svm = Support_Vector_Machine()
-                y_train_ovr = [None]*len(y_train)
-                y_test_ovr = [None]*len(y_test)
+                svm = SupportVectorMachine()
+                y_train_ovr = [None] * len(y_train)
+                y_test_ovr = [None] * len(y_test)
                 accuracies_pca = 0
-                print 'Running SVM. Please wait...'
+                print("Running SVM. Please wait...")
                 for i in range(1, 41):
                     # Setting the selected class as '1' and rest as '-1' depicting the One vs Rest classification.
                     for j in range(0, 320):
@@ -281,88 +289,93 @@ if __name__ == "__main__":
                     svm.train(X_train_pca, y_train_ovr)
                     predict_class = svm.predict(X_test_pca)
                     c = np.sum(predict_class == y_test_ovr)
-                    accuracies_pca += math.ceil(float(c)/len(predict_class)*100)
-                accuracy = accuracies_pca/40.0
-                print 'Accuracy is ', accuracy
+                    accuracies_pca += math.ceil(
+                        float(c) / len(predict_class) * 100)
+                accuracy = accuracies_pca / 40.0
+                print("Accuracy is ", accuracy)
                 avg_acc_svm_pca.append(accuracy)
                 if not All:
                     fld += 1
             if All:
                 fld += 1
 
-            print '\n'
+            print("\n")
         if DR == 'PCA' and Alg == '1NN' or All:
-            print 'Average accuracy for PCA + 1NN ', sum(avg_acc_p)/5.0, '\n'
+            print("Average accuracy for PCA + 1NN ",
+                  sum(avg_acc_p) / 5.0, "\n")
 
         if DR == 'LDA' and Alg == '1NN' or All:
-            print 'Average accuracy for LDA + 1NN', sum(avg_acc_l)/5.0, '\n'
+            print("Average accuracy for LDA + 1NN ",
+                  sum(avg_acc_l) / 5.0, "\n")
 
         if DR == 'PCA+LDA' and Alg == '1NN' or All:
-            print 'Average accuracy for PCA + LDA + 1NN', sum(avg_acc_pl)/5.0, '\n'
+            print("Average accuracy for PCA + LDA + 1NN ",
+                  sum(avg_acc_pl) / 5.0, "\n")
 
         if DR is None and Alg == 'SVM' or All:
-            print 'Average accuracy for SVM ', sum(avg_acc_svm)/5.0, '\n'
+            print("Average accuracy for SVM ", sum(avg_acc_svm) / 5.0, "\n")
 
         if DR == 'PCA' and Alg == 'SVM' or All:
-            print 'Average accuracy for PCA + SVM ', sum(avg_acc_svm_pca)/5.0, '\n'
-
+            print("Average accuracy for PCA + SVM ",
+                  sum(avg_acc_svm_pca) / 5.0, "\n")
 
     def task_selector():
-        print '\n'
-        choice = int(raw_input('1: PCA + 1NN \n'
-                               '2: Resized Images + PCA + 1NN \n'
-                               '3: LDA + 1NN \n'
-                               '4: PCA + LDA + 1NN \n'
-                               '5: SVM \n'
-                               '6: PCA + SVM \n'
-                               '7: Run all tasks \n'
-                               '0: To quit \n'
-                               'Please enter your choice: '))
-        print '\n'
+        print("\n")
+        choice = int(
+            input("1: PCA + 1NN \n"
+                  "2: Resized Images + PCA + 1NN \n"
+                  "3: LDA + 1NN \n"
+                  "4: PCA + LDA + 1NN \n"
+                  "5: SVM \n"
+                  "6: PCA + SVM \n"
+                  "7: Run all tasks \n"
+                  "0: To quit \n"
+                  "Please enter your choice: "))
+        print("\n")
         if choice == 1:
-            print "Running Task 1: PCA + 1NN"
+            print("Running Task 1: PCA + 1NN")
             X, y = img_input()
             cross_val(X, y, DR='PCA', Alg='1NN')
             task_selector()
 
         elif choice == 2:
-            print "Running Task 2: Resized Images + PCA + 1NN"
+            print("Running Task 2: Resized Images + PCA + 1NN")
             X, y = img_input(resize=True)
             cross_val(X, y, DR='PCA', Alg='1NN')
             task_selector()
 
         elif choice == 3:
-            print "Running Task 3: LDA + 1NN"
+            print("Running Task 3: LDA + 1NN")
             X, y = img_input()
             cross_val(X, y, DR='LDA', Alg='1NN')
             task_selector()
 
         elif choice == 4:
-            print "Running Task 4: PCA + LDA + 1NN"
+            print("Running Task 4: PCA + LDA + 1NN")
             X, y = img_input()
             cross_val(X, y, DR='PCA+LDA', Alg='1NN')
             task_selector()
 
         elif choice == 5:
-            print "Running Task 5: SVM\n"
+            print("Running Task 5: SVM\n")
             X, y = img_input()
             cross_val(X, y, DR=None, Alg='SVM')
             task_selector()
 
         elif choice == 6:
-            print "Running Task 6: PCA + SVM"
+            print("Running Task 6: PCA + SVM")
             X, y = img_input()
             cross_val(X, y, DR='PCA', Alg='SVM')
             task_selector()
 
         elif choice == 7:
-            print "Running All Tasks"
+            print("Running All Tasks")
 
-            print "Running Task 2: Resized Images + PCA + 1NN\n"
+            print("Running Task 2: Resized Images + PCA + 1NN")
             X, y = img_input(resize=True)
             cross_val(X, y, DR='PCA', Alg='1NN')
 
-            print "Running Rest of the Tasks, from 1,3-6 "
+            print("Running Rest of the Tasks, from 1,3-6")
             X, y = img_input()
             cross_val(X, y, All=True)
 
@@ -372,7 +385,7 @@ if __name__ == "__main__":
             quit()
 
         else:
-            print 'Please enter a valid choice'
+            print("Please enter a valid choice")
             task_selector()
 
     task_selector()
